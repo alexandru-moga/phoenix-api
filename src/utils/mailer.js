81720f0724e3183ceadc485784462508b89a9174
Alexandru Moga <alexandru.moga08@gmail.com>
2025-04-21 23:02:30 +0300

    Update mailer.js

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -4,7 +4,7 @@ async function sendLoginCode(email, code) {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
-        secure: true,
+        secure: false,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
